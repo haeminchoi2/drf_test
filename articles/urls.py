42ce182cb0d0c52bac2b0fd22ed4b7b85480cafe
haeminchoi2 <sjhk02@gmail.com>
2022-10-26 09:59:24 +0900

    Updated to CBV

--- articles/urls.py
+++ articles/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from articles import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', views.ArticleList.as_view(), name="ArticleList"),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,22 +1,21 @@
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from articles.serializers import ArticleSerializer
 from articles.models import Article
 # Create your views here.
 
 
-@api_view(['GET', 'POST'])
-def index(request):
-    if request.method == 'GET':
+class ArticleList(APIView):
+    def get(self, request, format=None):
         articles = Article.objects.all()
         serializer = ArticleSerializer(articles, many=True)
         return Response(serializer.data)
-    
-    if request.method == 'POST':
+
+    def post(self, request, format=None):
         serializer = ArticleSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
